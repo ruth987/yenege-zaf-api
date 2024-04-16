8fb32d3ecd24e58be45181c84b2a3d25d5020762
melak-12 <melakabebeee@gmail.com>
2024-04-16 18:39:07 +0300

    folder structure defined

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
 const port = process.env.PORT || 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello from your Node.js project!');
+app.get("/", (req, res) => {
+  res.send("Hello from your Node.js project!");
 });
 
 app.listen(port, () => {
