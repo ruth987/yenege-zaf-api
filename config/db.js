59aeb7a499860f16c59547219f9bd32c89601612
Ruth Wossen <ruthwossen75@gmail.com>
2024-04-17 16:40:47 +0300

    fix

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const { MONGO_URI } = process.env;
 
 exports.connect = () => {
-    console.log(MONGO_URI);
 
     mongoose.connect(MONGO_URI)
             .then(() => console.log('MongoDB connected successfully'))
-            .catch(err => console.error('Error connecting to MongoDBgit
+            .catch(err => console.error('Error connecting to MongoDBgit'))
+}
